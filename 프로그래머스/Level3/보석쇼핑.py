680311a3397dd3339f509a98c29ebf31192fe532
kyungkoh <kyungkoh1328@naver.com>
2021-10-04 16:24:35 +0900

    Add Python PS : binary Search

---
+++ 프로그래머스/Level3/보석쇼핑.py
@@ -0,0 +1,28 @@
+#1004
+from collections import defaultdict 
+def solution(gems):
+    answer = []
+    dic = {gems[0]:1}
+    size = len(set(gems))
+    temp = [0,len(gems)-1]
+    start , end = 0 , 0
+    
+    while start < len(gems) and end < len(gems):
+        if len(dic) == size :
+                if end - start < temp[1] - temp[0]:
+                    temp[0] , temp[1] = start , end
+                if dic[gems[start]] == 1: 
+                    del dic[gems[start]]
+                else:
+                    dic[gems[start]] -=1
+                start += 1
+        else:
+            end +=1 
+            if end == len(gems):
+                break
+            if gems[end] in dic.keys():
+                dic[gems[end]] +=1
+            else:
+                dic[gems[end]] = 1 
+    
+    return [temp[0]+1, temp[1]+1]
